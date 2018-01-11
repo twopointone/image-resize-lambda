026bf3cd057ab3df658847ebd5d3f694871debc6
harshal27 <harshaljain27@gmail.com>
2018-01-11 17:01:39 +0530

    changing raw storage format

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ exports.handler = function(event, context, callback) {
         if (imageProcessor && params) {
             ProcessImage.processImage(key, params, processCallback);
         } else if (rawProcessor) {
-            ProcessRaw.processRaw(processorData.path.split('/')[1], key.replace('/',''), processCallback);
+            ProcessRaw.processRaw(processorData.path, key.replace('/',''), processCallback);
         } else {
             console.log("Image processor or raw processor not found in request.");
             callback(null, {
